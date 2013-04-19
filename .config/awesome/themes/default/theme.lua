1fdba005169190b0759681de056edc25c08513bb
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2013-04-19 14:17:04 -0700

    Awesome config updated.

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -73,6 +73,7 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
+theme.wallpaper = ".config/awesome/themes/default/wallpaper.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
