919f629513078b79a3fbf0d1d5674503e2af8f8f
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-09-12 15:07:43 +0100

    Add gnu repository.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 
 (require 'package)
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("gnu" .  "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (package-initialize)
